0fcca5a464f102a6dc0cbaeb6dc38b2a35579b3d
nicolas <nicolasemanuelfernadnez@gmail.com>
2020-03-21 12:10:32 -0300

    Updates

---
+++ precache-manifest.034214241ba88b43595cea89c86332ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f3a564465982a3f6111e5ce2e98556",
+    "url": "/marvel-react/index.html"
+  },
+  {
+    "revision": "67f2c0ee8c43c4c37933",
+    "url": "/marvel-react/static/css/main.e8d245e3.chunk.css"
+  },
+  {
+    "revision": "b4d0c2f08bbbaac0e496",
+    "url": "/marvel-react/static/js/2.d2abd1b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marvel-react/static/js/2.d2abd1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f2c0ee8c43c4c37933",
+    "url": "/marvel-react/static/js/main.efa4dd88.chunk.js"
+  },
+  {
+    "revision": "3b0fd13f39991e4a386b",
+    "url": "/marvel-react/static/js/runtime-main.ff0a5761.js"
+  }
+]);
